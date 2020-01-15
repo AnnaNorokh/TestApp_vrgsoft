c4997ee1da5beb581ccf9a60ae8d3fe6449db6df
Anna Norokh <annanorokh15@gmail.com>
2020-01-16 00:45:04 +0200

    v1

--- app/src/main/java/com/example/planing/testapp_vrgsoft/MainActivity.java
+++ app/src/main/java/com/example/planing/testapp_vrgsoft/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.planing.testapp_vrgsoft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.planing.testapp_vrgsoft.package1.Feed;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
+import static com.example.planing.testapp_vrgsoft.FeedAPI.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .build();
+
+        FeedAPI feedAPI = retrofit.create(FeedAPI.class);
+
+        Call<Feed> call = feedAPI.getFeed();
+
+        call.enqueue(new Callback<Feed>() {
+            @Override
+            public void onResponse(Call<Feed> call, Response<Feed> response) {
+                Log.d(TAG, "Response: feed: " + response.body().toString());
+                Log.d(TAG, "Response: Server Response: " + response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<Feed> call, Throwable t) {
+                Log.e(TAG, "Failure: Unable to retrieve RSS: " + t.getMessage() );
+                Toast.makeText(MainActivity.this, "An Error Occured", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
     }
 }
